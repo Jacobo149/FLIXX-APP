995bdafb44e0f675a7382f9db71762685ed1fbb4
Jacobo149 <jtp323@lehigh.edu>
2023-10-31 16:38:34 -0400

    Popular Movies Implemented

--- js/script.js
+++ js/script.js
@@ -3,6 +3,51 @@ const global = {
 };
 
 
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');
+    
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+                movie.poster_path 
+                ? `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+              />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+
+        document.querySelector('#popular-movies').appendChild(div);
+    });
+}
+
+// Fetching data from TMDB API
+async function fetchAPIData(endpoint) {
+    const API_KEY = '70430c9a892acb0576373d79e9f929a3';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+
+    const data = await response.json();
+
+    return data;
+}
+
 // Highlight active menu item
 function highlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
@@ -20,6 +65,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
+            displayPopularMovies();
             break;
         case '/shows.html':
             break;
@@ -30,7 +76,7 @@ function init() {
         case '/search.html':
             break;
     }
-    
+
     highlightActiveLink();
 }
 
