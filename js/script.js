ef7effd15a0caab5960f670dead7fd760d54c692
Jacobo149 <jtp323@lehigh.edu>
2023-10-31 20:25:38 -0400

    Background Image

--- js/script.js
+++ js/script.js
@@ -76,6 +76,9 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIData(`movie/${movieId}`);
 
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
 
     div.innerHTML = `<div class="details-top">
@@ -127,6 +130,29 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 
 }
+
+//Display Backdrop On Details Page
+function displayBackgroundImage(type, backdropPath) {
+   const overlayDiv = document.createElement('div');
+   overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`;
+   overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+}
+
 
 // Fetching data from TMDB API
 async function fetchAPIData(endpoint) {
